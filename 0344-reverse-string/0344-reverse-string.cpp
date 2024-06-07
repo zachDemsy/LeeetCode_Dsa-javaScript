b12823d459efeffbf4a883256eab677009f9161e
Pulkit Pandey <115865402+zachDemsy@users.noreply.github.com>
2024-06-07 23:12:11 +0530

    Time: 16 ms (66.78%), Space: 26.9 MB (89.64%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while (left < right) {
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            
+            left++;
+            right--;
+        }
+    }
+};
