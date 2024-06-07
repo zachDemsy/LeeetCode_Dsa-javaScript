96874cf2e9f1aaf6fe61eca4cccfa6d822a1a226
Pulkit Pandey <115865402+zachDemsy@users.noreply.github.com>
2024-06-07 23:03:14 +0530

    Time: 13 ms (60.37%), Space: 11.2 MB (49.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -10,14 +10,11 @@ public:
         int start = 0;
         
         for (int end = 0; end < s.length(); end++) {
-            // If the character is found in the map and its index is after the start of the current substring,
-            // update the start index to the index after the last occurrence of the character.
+            
             if (charIndexMap.find(s[end]) != charIndexMap.end() && charIndexMap[s[end]] >= start) {
                 start = charIndexMap[s[end]] + 1;
             }
-            // Update the index of the current character in the map.
             charIndexMap[s[end]] = end;
-            // Update the maximum length of the substring.
             maxLength = std::max(maxLength, end - start + 1);
         }
         
